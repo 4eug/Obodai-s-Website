c4422daf6efaca6bc2a9aca46fe1f5ca5414a84e
4eug <debraheug@gmail.com>
2022-08-14 10:29:29 +0000

    mini-navbar

---
+++ components/NavBar/mini-nav.jsx
@@ -0,0 +1,92 @@
+import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+
+const Mininav = () => {
+  const [nav, setNav] = useState(false);
+  const [color, setColor] = useState('transparent');
+  const [textColor, setTextColor] = useState('white');
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  useEffect(() => {
+    const changeColor = () => {
+      if (window.scrollY >= 90) {
+        setColor('#ffffff');
+        setTextColor('#000000');
+      } else {
+        setColor('transparent');
+        setTextColor('#ffffff');
+      }
+    };
+    window.addEventListener('scroll', changeColor);
+  }, []);
+
+  return (
+    <nav
+      style={{ backgroundColor: `${color}` }}
+      className='fixed left-0 top-0 w-full z-10 ease-in duration-300'
+    >
+      <div className=' m-auto flex justify-between items-center p-4 text-white'>
+       
+        <ul style={{ color: `${textColor}` }} className='hidden sm:flex'>
+          <li className='p-4'>
+            <Link href='/'>ABOUT</Link>
+          </li>
+          <li className='p-4'>
+            <Link href='/#gallery'>WORK</Link>
+          </li>
+          <li className='p-4'>
+            <Link href='/work'>CAPABILITIES</Link>
+          </li>
+          
+        </ul>
+
+        <ul>
+        <li className='p-4 hidden md:flex '>
+          <button style={{ color: `${textColor}` }} className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-white border border-{`${textColor}`} dark:hover:bg-black"> START A PROJECT </button>
+          </li>
+        </ul>
+
+       
+
+        {/* Mobile Button */}
+         <div onClick={handleNav} className='block sm:hidden z-10'>
+          {nav ? (
+            <AiOutlineClose size={20} style={{ color: `${textColor}` }} />
+          ) : (
+            <AiOutlineMenu size={20} style={{ color: `${textColor}` }} />
+          )}
+        </div>
+        {/* Mobile Menu */}
+        <div
+          className={
+            nav
+              ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+              : 'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+          }
+        > 
+          <ul>
+            <li onClick={handleNav} className='p-4 text-2xl hover:text-gray-500'>
+              <Link href='/'>ABOUT</Link>
+            </li>
+            <li onClick={handleNav} className='p-4 text-2xl hover:text-gray-500'>
+              <Link href='/#gallery'>WORK</Link>
+            </li>
+            <li onClick={handleNav} className='p-4 text-2xl hover:text-gray-500'>
+              <Link href='/work'>CAPABILITIES</Link>
+            </li>
+            <button onClick={handleNav} className='py-4 px-5 mr-2 mb-2 text-2xl mt-4 font-medium text-white border border-white dark:hover:bg-black'>
+              <Link href='/work'>START A PROJECT </Link>
+            </button>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Mininav;

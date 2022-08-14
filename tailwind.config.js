31b25f78c9b720397d24d7519cdf723af2507dbe
4eug <debraheug@gmail.com>
2022-08-14 10:10:55 +0000

    Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}",
   "./components/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        custom: ['Montserrat', 'sans-serif'],
+      },
+      fontSize: {
+      'obodai-header-h1': '64px',
+      'obodai-header-h2': '48px',
+      'obodai-header-h3': '36px',
+      'obodai-header-h4': '24px',
+      'obodai-header-h5': '20px',
+      'obodai-header-h6': '16px',
+      }
+    },
     
   },
   plugins: [],

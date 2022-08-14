1f16f605d64e0674a388434961c737be20875178
4eug <debraheug@gmail.com>
2022-08-14 10:11:10 +0000

    Contact Us Page

---
+++ pages/contact.jsx
@@ -0,0 +1,11 @@
+import Contacts from "../components/Contact-Us/Contacts"
+
+const Contact = () => {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Contacts/>
+    </div>
+  )
+}
+
+export default Contact

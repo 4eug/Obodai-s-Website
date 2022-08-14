fe528bfa68c0128416c17f25b83bd2bb75995669
4eug <debraheug@gmail.com>
2022-08-14 10:29:37 +0000

    Contact Us Page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,9 +1,14 @@
 import Contacts from "../components/Contact-Us/Contacts"
+import Mininav from "../components/NavBar/mini-nav"
 
 const Contact = () => {
   return (
     <div className="max-w-7xl mx-auto">
-      <Contacts/>
+    <Mininav/>
+    <section className='md:mt-32 mt-16'>
+    <Contacts/>
+    </section>
+      
     </div>
   )
 }

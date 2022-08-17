2ecb929e16e2a3d3d52816f0e696ee2c15350510
4eug <debraheug@gmail.com>
2022-08-17 17:31:00 +0000

    Strategy

---
+++ components/Message/strategy-message.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+
+
+const Strategy = () => {
+  return (
+      <div className="p-4 md:p-1 flex flex-col justify-between">
+        <div className="grid md:grid-cols-2 max-w-[1240px] m-2 md:m-8">
+          <div className="flex flex-col justify-center md:items-start w-full py-8">
+            <h1 className="text-4xl md:text-4xl font-bold ">We’re a strategy-led design studio.</h1>
+            <p className="mt-2 text-2xl">
+            Partnering with bold, forward-thinking
+            businesses to create identities & experiences
+            that drive transformation and growth.
+            </p>
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default Strategy;

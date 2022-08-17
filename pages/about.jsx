ca04d826162379e961d75340756436635a82f0f0
4eug <debraheug@gmail.com>
2022-08-17 18:08:23 +0000

    About Page

---
+++ pages/about.jsx
@@ -0,0 +1,16 @@
+import Footer from "../components/Footer/footer"
+import Mininav from "../components/NavBar/mini-nav"
+
+const About = () => {
+  return (
+    <div>
+    <Mininav/>
+    {/* <section className='mt-10'>
+    <Footer/>
+    </section> */}
+      
+    </div>
+  )
+}
+
+export default About;

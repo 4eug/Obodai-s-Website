e5f3d3170718984c77e8ac739d4762cb95d72708
4eug <debraheug@gmail.com>
2022-08-17 17:59:01 +0000

    Lets Talk About You

--- components/Let's Talk/lets-talk.jsx
+++ components/Let's Talk/lets-talk.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const LetsTalk = () => {
   return (
-      <div className=" md:p-1 flex flex-col justify-between">
+      <div className="p-4 md:p-1 flex flex-col justify-between">
         <div className="grid md:grid-cols-2 max-w-[1240px] m-2 md:m-8">
           <div className="flex flex-col justify-center md:items-start w-full">
-            <h1 className="text-4xl md:text-4xl font-bold ">We’re a strategy-led design studio.</h1>
+            <h1 className="text-4xl md:text-4xl font-bold ">Let’s talk about you</h1>
             <div className="mt-2 text-2xl">
-                More about us
+            Get in touch
             </div>
           </div>
         </div>

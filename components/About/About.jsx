9e7c1075983d3394a67686d309725efd29ba0966
4eug <debraheug@gmail.com>
2022-08-27 09:25:57 +0000

    About

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React, {useState} from "react";
 
-const AboutHead = () => {
+const AboutHead = (props) => {
   return (
     <div name="home" className="w-full h-full flex flex-col justify-between ">
       <div className="grid md:grid-cols-1 max-w-auto ml-2 md:ml-32 mt-16 md:mt-24">
@@ -9,17 +9,16 @@ const AboutHead = () => {
           className="flex flex-col justify-center md:items-start 2xl:pl-24 2xl:mb-28 w-full px-2 py-8 "
         >
           <h1 className="text-xl md:text-xl font-bold">
-          ABOUT
+          {props.heading}
           </h1>
           <h1 className="text-4xl md:text-6xl font-bold mt-2">
-          For bold, forward-
+          {props.title}
           </h1>    
           <h1 className="text-4xl md:text-6xl font-bold mt-2">
-          thinking businesses.
+          {props.title2}
           </h1>
         </div>
       </div>
-      <hr className="my-2 mx-4 border-gray-700 sm:mx-auto lg:my-4 lg:mx-32"/>
     </div>
   );
 };

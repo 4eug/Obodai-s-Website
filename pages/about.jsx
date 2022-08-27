165b29b79fa4979578aeb9de86e5243d2f40ab4d
4eug <debraheug@gmail.com>
2022-08-27 08:41:52 +0000

    About

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,13 +1,23 @@
+import AboutHead from "../components/About/About";
+import Brands from "../components/Brands/Brands";
 import Footer from "../components/Footer/footer"
+import LetsTalk from "../components/Let's Talk/lets-talk";
 import Mininav from "../components/NavBar/mini-nav"
 
 const About = () => {
   return (
     <div>
     <Mininav/>
-    {/* <section className='mt-10'>
+    <section>
+     <AboutHead/>
+    </section>
+    <Brands/>
+    <section className='bg-gray-100 py-2'>
+      <LetsTalk/>
+    </section>
+    <section>
     <Footer/>
-    </section> */}
+    </section>
       
     </div>
   )

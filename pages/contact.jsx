5fc4781340ebedf4699816211407527d444870f4
4eug <debraheug@gmail.com>
2022-09-04 09:33:33 +0000

    HeadNav

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,13 +1,13 @@
 import Contacts from "../components/Contact-Us/Contacts"
 import Footer from "../components/Footer/footer"
-import Mininav from "../components/NavBar/mini-nav"
+import Headnav from "../components/NavBar/head-nav"
 import Meta from "../components/utilities/meta/meta"
 
 const Contact = () => {
   return (
     <div>
       <Meta title="Contact" />
-    <Mininav/>
+    <Headnav/>
     <section className='md:mt-32 mt-16'>
     <Contacts/>
     </section>

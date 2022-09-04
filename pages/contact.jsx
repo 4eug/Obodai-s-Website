4db3b96578eadefe43ccd33c7e82880a8fdb903f
4eug <debraheug@gmail.com>
2022-09-04 10:54:49 +0000

    Changes

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,13 +1,13 @@
 import Contacts from "../components/Contact-Us/Contacts"
 import Footer from "../components/Footer/footer"
-import Headnav from "../components/NavBar/head-nav"
+import Mininav from "../components/NavBar/mini-nav"
 import Meta from "../components/utilities/meta/meta"
 
 const Contact = () => {
   return (
     <div>
       <Meta title="Contact" />
-    <Headnav/>
+      <Mininav/>
     <section className='md:mt-32 mt-16'>
     <Contacts/>
     </section>

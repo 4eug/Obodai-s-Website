d6f5ff4725d5cb62eddb13faa8701ad7e7894d78
4eug <debraheug@gmail.com>
2022-09-04 09:59:17 +0000

    Contact

--- components/Contact-Us/Contacts.jsx
+++ components/Contact-Us/Contacts.jsx
@@ -6,7 +6,7 @@ const Contacts = () => {
             Hello,
             </h1>
     
-            <h5 className='text-obodai-header-h5 pl-1 leading-loose'>Ready to create some beautiful magic together? Let’s Talk.</h5>
+            <h5 className='text-obodai-header-h5 pl-1 w-full md:w-3/4 leading-loose'>Ready to create some beautiful magic together? Let’s Talk.</h5>
     
           </div>
           <div className='w-full sm:w-2/3 lg:pr-20'>
@@ -90,7 +90,7 @@ const Contacts = () => {
     
               <div className='my-5'>
               <a href="" className="mt-2">
-            <span className="text-2xl md:text-3xl font-light">Submit</span>
+            <span className="text-2xl font-light">Submit</span>
 
             <svg width="72" height="24" viewBox="0 0 72 24" className="inline-block pb-2" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M71.0607 13.0607C71.6464 12.4749 71.6464 11.5251 71.0607 10.9393L61.5147 1.3934C60.9289 0.807611 59.9792 0.807611 59.3934 1.3934C58.8076 1.97919 58.8076 2.92893 59.3934 3.51472L67.8787 12L59.3934 20.4853C58.8076 21.0711 58.8076 22.0208 59.3934 22.6066C59.9792 23.1924 60.9289 23.1924 61.5147 22.6066L71.0607 13.0607ZM0 13.5H70V10.5H0V13.5Z" fill="black"/>

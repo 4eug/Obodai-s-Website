3f81b86ec055fdae28f6d176cbacbded17f5c588
4eug <debraheug@gmail.com>
2022-09-18 08:11:18 +0000

    Hydration fixing

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import React, { useEffect, useState } from 'react'
+
 
 function MyApp({ Component, pageProps }) {
+  const [showChild, setShowChild] = useState(false);
+
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if(!showChild) {
+    return null;
+  }
+
+  if(typeof window === 'undefined') {
+    return <></>;
+  } else {
+
   return <Component {...pageProps} />
+}
 }
 
 export default MyApp

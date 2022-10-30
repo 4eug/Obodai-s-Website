9f0562c6d04ab4c8d3432ba6aa950178dc856775
4eug <debraheug@gmail.com>
2022-10-30 12:38:12 +0000

    Links Added

--- components/Footer/footer.jsx
+++ components/Footer/footer.jsx
@@ -21,9 +21,9 @@ const Footer = ({heading}) => {
                             <span className='mb-8'><a href="mailto:hello@obodai.com" className="text-white text-xl">hello@obodai.com</a></span>
                         </div>
                         <div className="flex flex-col">
-                            <span><a href="#" className="text-white text-xl">Instagram</a></span>
-                            <span className="my-2"><a href="#" className="text-white text-xl">Twitter</a></span>
-                            <span><a href="#" className="text-white text-xl my-2">LinkedIn</a></span>
+                            <span><a href="https://www.instagram.com/weareobodai/" className="text-white text-xl">Instagram</a></span>
+                            <span className="my-2"><a href="https://twitter.com/weareobodai" className="text-white text-xl">Twitter</a></span>
+                            <span><a href="https://www.linkedin.com/company/weareobodai/" className="text-white text-xl my-2">LinkedIn</a></span>
                         </div>
                 
                     </div>

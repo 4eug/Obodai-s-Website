ac6b5dd21a629a53875229646837923dd03ad143
4eug <debraheug@gmail.com>
2022-10-30 12:44:18 +0000

    Contact

--- components/Contact-Us/Contacts.jsx
+++ components/Contact-Us/Contacts.jsx
@@ -1,19 +1,35 @@
+/* eslint-disable react/jsx-no-undef */
 const Contacts = () => { 
     return (
         <div className='w-full flex items-start justify-center flex-wrap pb-4 sm:pb-10 md:pb-12 lg:pb-16 px-4 lg:pl-10'>
           <div className='w-full sm:w-1/3 pb-8 sm:pb-0 sm:pl-0 lg:pl-10'>
-            <h1 className='text-left text-obodai-header-h1 lg:text-obodai-header-h2 font-bold leading-snug'>
+            <h1 className='text-left text-[64px] lg:text-[96px] font-bold leading-snug'>
             Hello,
             </h1>
     
-            <h5 className='text-obodai-header-h5 pl-1 w-full md:w-3/4 leading-loose'>Ready to create some beautiful magic together? Let’s Talk.</h5>
+            <h5 className='text-[24px] pl-1 w-full md:w-3/4 leading-[29px]'>
+            Ready to create some
+            beautiful magic together? 
+            Schedule a brief consult 
+            <br className="hidden md:flex" />
+            with us.
+              </h5>
+
+            <h5 className="mt-[35px] text-[24px] leading-[29px] pl-1">
+            Do you have a general <br />
+            question?
+            <p className="mt-[20px] pl-1">
+              Email us instead.
+            </p>
+            </h5>
+
     
           </div>
           <div className='w-full sm:w-2/3 lg:pr-20'>
             <form
             >
-                <h1 className="text-obodai-header-h3 font-bold">START PROJECT</h1>
-              <div className='mt-5 w-full flex justify-between flex-wrap mb-6'>
+                <h1 className="text-obodai-header-h3 font-bold">WORK WITH US</h1>
+              <div className='mt-1 w-full flex justify-between flex-wrap mb-6'>
                 <div className='w-full sm:w-1/2 sm:pr-3'>
                   <input
                     type='text'
@@ -89,14 +105,17 @@ const Contacts = () => {
               </div>
     
               <div className='my-5'>
-              <a href="" className="mt-2">
+              
+            <button className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-white bg-black">
+            <a href="" className="mt-2">
             <span className="text-2xl font-light">Submit</span>
 
             <svg width="72" height="24" viewBox="0 0 72 24" className="inline-block pb-2" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M71.0607 13.0607C71.6464 12.4749 71.6464 11.5251 71.0607 10.9393L61.5147 1.3934C60.9289 0.807611 59.9792 0.807611 59.3934 1.3934C58.8076 1.97919 58.8076 2.92893 59.3934 3.51472L67.8787 12L59.3934 20.4853C58.8076 21.0711 58.8076 22.0208 59.3934 22.6066C59.9792 23.1924 60.9289 23.1924 61.5147 22.6066L71.0607 13.0607ZM0 13.5H70V10.5H0V13.5Z" fill="black"/>
+            <path d="M71.0607 13.0607C71.6464 12.4749 71.6464 11.5251 71.0607 10.9393L61.5147 1.3934C60.9289 0.807611 59.9792 0.807611 59.3934 1.3934C58.8076 1.97919 58.8076 2.92893 59.3934 3.51472L67.8787 12L59.3934 20.4853C58.8076 21.0711 58.8076 22.0208 59.3934 22.6066C59.9792 23.1924 60.9289 23.1924 61.5147 22.6066L71.0607 13.0607ZM0 13.5H70V10.5H0V13.5Z" fill="white"/>
         </svg>
   
             </a>
+          </button>
               </div>
             </form>
           </div>

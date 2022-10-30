614068501d728e9911de83b0a619d2c61d4cc995
4eug <debraheug@gmail.com>
2022-10-30 12:40:42 +0000

    Case studies

--- components/Projects/works/caseStudies.jsx
+++ components/Projects/works/caseStudies.jsx
@@ -5,27 +5,34 @@ import WorkCard from "../../utilities/cards/workCard";
 export const CaseStudies = () => {
     const studiesArray = [
         {
-          title: "KUPFUL",
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-          imgSrc: "/Kupful.jpeg",
-          type: "Identity Design/Positioning",
+          title: "Carpe Diem Beads & Beauty ",
+          description: "After some months of operating, Carpe Diem was evolving from the bead making business it was into a beauty brand. Their current logo didn't reflect the brand's new direction, so they approached us to help give the brand a facelift.",
+          imgSrc: "/Carpe Diem.jpeg",
           id: 0,
         },
         {
-          title: "Mylo & Eddie",
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-          imgSrc: "/Mylo & Eddie.png",
+          title: "Extraterrestrial Arts, LLC.",
+          description: "A talent management company on a mission to help young African talents reach their full potentials, and give them a platform to attain a global level of acclaim. They approached us to help create a brand that will reflect this ambition.",
+          imgSrc: "/ETA.jpeg",
           lgFlexDirection: "lg:flex-row-reverse",
           type: "Identity Design/Positioning",
           id: 1,
           
         },
         {
-          title: "EXTRATERRESTRIAL ARTS",
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-          imgSrc: "/ETA.png", 
+          title: "Wall of Fave",
+          description: "A curated collection of our favorite logos among the identities we crafted for brands across various industries, between 2019 - 2021. This collection consists of logotypes, symbols & combination marks. This is for the brand thinker looking some logo inspiration. Enjoy!",
+          imgSrc: "/Wall of Fave.jpeg", 
           type: "Identity Design/Positioning",
           id: 2,
+        },
+        {
+          title: "Marvin's Touch",
+          description: "Coming Soon",
+          imgSrc: "/ETA.png", 
+          lgFlexDirection: "lg:flex-row-reverse",
+          type: "Identity Design/Positioning",
+          id: 3,
         },
         
       ];

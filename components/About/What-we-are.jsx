8692d5511cc1d14cc6b4811bc737914a206f581b
4eug <debraheug@gmail.com>
2022-10-30 12:41:20 +0000

    About

--- components/About/What-we-are.jsx
+++ components/About/What-we-are.jsx
@@ -3,28 +3,32 @@ const WhatWeAre = () => {
         <div className='w-full flex items-start mt-10 justify-center flex-wrap pb-4 sm:pb-10 md:pb-12 lg:pb-16 px-4 lg:pl-10'>
           <div className='w-full sm:w-1/3 pb-8 sm:pb-0 sm:pl-0 lg:pl-10'>
             <h1 className='ml-0 md:ml-0 text-left text-3xl lg:text-4xl font-bold leading-snug'>
-            We are OBODAI
+            We are brand thinkers.
             </h1>
           </div>
           <div className='w-full sm:w-2/3 lg:pr-20'>
            <p className='ml-0 md:ml-32 text-left text-xl lg:text-2xl leading-snug'>
-           We’re a strategy-led design studio focused on
-        creating noteworthy identities & experiences that
-        drive transformation and growth.
+           In an age of abundant choice and information, people 
+           expect more of brands than ever before. It has therefore
+          become necessary for businesses to tell clear & distinct
+          stories in order to cut through the noise and be seen &
+           heard. And now, businesses are only as strong as their brands.
+
 
         <p className="mt-10">
-        We partner with bold, forward-thinking
-        businesses to build memorable brands that
-        create a meaningful impact in the world.
+        OBODAI is a boutique brand design firm focused on
+        creating distinctive identities and clarity for bold, forward-
+        looking businesses. We innovate at the intersection of
+        design and brand thinking to refresh boring or slow
+        brands and create transformative solutions for brand growth.
+
         </p>
 
         <p className="mt-10">
-        People expect more of brands than ever before,
-        and now businesses are only as strong as their
-        brands. We innovate at the intersection of
-        strategy and design thinking to bring forth the
-        best possible transformative solutions for our
-        clients.
+        Our goal is to help you make a lasting impression on your
+        audience, gain a competitive advantage, and build an enduring
+        relationship with your tribe. Put differently, we are here to help
+        you win hearts and turn these hearts into loyal customers.
         </p>
         
            </p>

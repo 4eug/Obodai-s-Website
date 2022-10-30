55912d5f01ab9bc81cb983c2f191e7f829cb4a50
4eug <debraheug@gmail.com>
2022-10-30 10:30:25 +0000

    Startegy Message

--- components/Message/strategy-message.jsx
+++ components/Message/strategy-message.jsx
@@ -6,20 +6,20 @@ const Strategy = () => {
       <div className="p-4 md:p-1 flex flex-col justify-between">
         <div className="grid md:grid-cols-2 max-w-[1240px] m-2 md:m-8">
           <div className="flex flex-col justify-center md:items-start w-full py-8">
-            <h1 className="text-4xl md:text-4xl font-bold ">We’re a strategy-led design studio.</h1>
-            <p className="mt-2 text-3xl font-light">
-            Partnering with bold, forward-thinking
-            businesses to create identities & experiences
-            that drive transformation and growth.
+            {/* <h1 className="text-4xl md:text-4xl font-bold ">We’re a strategy-led design studio.</h1> */}
+            <p className="mt-2 text-[30px] md:text-[40px] leading-tight font-light">
+            We work with business leaders to help
+            drive brand transformation and create
+            unique stories that inspire.
             </p>
-            <a href="" className="mt-4">
+            {/* <a href="" className="mt-4">
             <span className="text-2xl font-light">More about us</span>
 
             <svg width="72" height="24" viewBox="0 0 72 24" className="inline-block pb-2" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M71.0607 13.0607C71.6464 12.4749 71.6464 11.5251 71.0607 10.9393L61.5147 1.3934C60.9289 0.807611 59.9792 0.807611 59.3934 1.3934C58.8076 1.97919 58.8076 2.92893 59.3934 3.51472L67.8787 12L59.3934 20.4853C58.8076 21.0711 58.8076 22.0208 59.3934 22.6066C59.9792 23.1924 60.9289 23.1924 61.5147 22.6066L71.0607 13.0607ZM0 13.5H70V10.5H0V13.5Z" fill="black"/>
         </svg>
   
-            </a>
+            </a> */}
           </div>
         </div>
       </div>
